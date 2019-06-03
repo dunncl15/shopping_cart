99bf2ead12edb9285984a3fe95133ea84464db32
Charles Dunn <dunnc1551@gmail.com>
2019-06-03 12:48:16 -0600

    Add basic error handling. Remove cart data transform.

--- api/server.js
+++ api/server.js
@@ -12,31 +12,37 @@ app.use(bodyParser.json());
 // Product API
 // GET all products
 app.get('/api/products', (req, res) => {
-  Product.findAll().then(products => res.json(transformData(products)));
+  Product.findAll()
+    .then(products => res.json(transformData(products)))
+    .catch(error => res.send(error));
 });
 
 // Cart API
 // GET all cart items
 app.get('/api/cart', (req, res) => {
-  Cart.findAll().then(items => res.json(transformData(items)));
+  Cart.findAll()
+    .then(items => res.json(items))
+    .catch(error => res.send(error));
 });
 
 // Add new item to cart
 app.post('/api/cart', (req, res) => {
-  const { id, quantity } = req.body;
-  Cart.create({ id, quantity }).then(() => {
-    Cart.findAll().then(items => res.json(transformData(items)));
-  });
+  const { id, quantity = 1 } = req.body;
+  Cart.create({ id, quantity })
+    .then(item => res.json(item))
+    .catch(error => res.send(error));
 });
 
 // Update item quantity by id
 app.put('/api/cart', (req, res) => {
   const { id, quantity } = req.body;
-  Cart.findByPk(id).then(item => {
-    item.update({ quantity }).then(() => {
-      Cart.findAll().then(items => res.json(transformData(items)));
-    });
-  });
+  Cart.findByPk(id)
+    .then(item => {
+      item.update({ quantity }).then(() => {
+        Cart.findAll().then(items => res.json(items));
+      });
+    })
+    .catch(error => res.send(error));
 });
 
 init().then(() => {
